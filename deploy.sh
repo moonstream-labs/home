f4b6aebfeb40f95f8d6c64c6cf84e733f07c29c2
moonstream-labs <ryan@moonstream.xyz>
2024-03-16 10:32:40 -0400

    Test Icicle

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+
+# Declare a variable for the custom commit message
+commit_message="$1"
+
+# Check if the commit_message variable is empty
+if [ -z "$commit_message" ]; then
+  echo "Please provide a commit message."
+  exit 1
+fi
+
+# Run the nue build command
 nue build -r labs --production
+
+# Add all changes to the staging area
 git add -A
-git commit -m "🧟‍♂️"
+
+# Commit the changes with the custom commit message
+git commit -m "$commit_message"
+
+# Push the changes to the remote repository
 git push
